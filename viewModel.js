ff39de39fecc1638988dfac41b5f104d835c8982
José Miguel Malaca <jose.miguel.malaca@me.com>
2015-04-22 12:37:20 +0100

    Version 2.0...
    There was a problem with the Version 1.0... 
    Catching the "change" event and the "keyup" event results in two requests... that's wrong... we just one one request per change in the text.

--- viewModel.js
+++ viewModel.js
@@ -14,36 +14,38 @@ kendo.data.binders.valueInserted = kendo.data.Binder.extend({
         
         var that = this;
         //listen for the change event of the element
-        $(that.element).on("change", function() {
+        //$(that.element).on("change", function() {
             //call the change function
-            that.change();
-        });
+        //    that.change();
+        //});
         
         //listen for the keyup event of the element
         var binding = this.bindings.keyPress;
         $(that.element).bind("keyup", function(){
             delay(function(){
                 //call the change function
-                that.change();
-            }, 1500 );
+                that.refresh();
+            }, 1000 );
         });
     },
     
     refresh: function() {
-        var that = this,
-            //get the value from the View-Model
-            value = that.bindings["valueInserted"].get();
-        
+        //var that = this,
+        //get the value from the View-Model
+        //value = that.bindings["valueInserted"].get();
         //update the HTML input element
-        $(that.element).val(value);
-    },
-    
-    change: function() {
-        var value = this.element.value;
+        //$(that.element).val(value);
         
+        var value = this.element.value;    
         //update the View-Model
         this.bindings["valueInserted"].set(value);
-    }
+    }//,
+    
+    //change: function() {
+    //    var value = this.element.value;
+    //    update the View-Model
+    //    this.bindings["valueInserted"].set(value);
+    //}
 });
 
 //View-Model source
