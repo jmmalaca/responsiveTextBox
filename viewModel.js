c509659979658d1b90e351b59b476d0513ff6980
José Miguel Malaca <jose.miguel.malaca@me.com>
2015-04-24 13:00:03 +0100

    Update to a new version using the RIGHT events...
    In the last version there was an error that I found after... the user pasting something with the mouse and not ctrL+C... the event was not catch... now I think everything is OK.

--- viewModel.js
+++ viewModel.js
@@ -6,47 +6,63 @@ var delay = (function(){
   };
 })();
 
+//Version 2.0
 kendo.data.binders.valueInserted = kendo.data.Binder.extend({
-    
     init: function(element, bindings, options) {
-        //call the base constructor
         kendo.data.Binder.fn.init.call(this, element, bindings, options);
-        
         var that = this;
-        //listen for the change event of the element
-        //$(that.element).on("change", function() {
-            //call the change function
-        //    that.change();
-        //});
-        
-        //listen for the keyup event of the element
-        var binding = this.bindings.keyPress;
-        $(that.element).bind("keyup", function(){
+        $(that.element).bind("input propertychange paste", function(){
             delay(function(){
-                //call the change function
                 that.refresh();
-            }, 1000 );
+            }, 2000 );
         });
     },
-    
     refresh: function() {
-        //var that = this,
-        //get the value from the View-Model
-        //value = that.bindings["valueInserted"].get();
-        //update the HTML input element
-        //$(that.element).val(value);
-        
-        var value = this.element.value;    
-        //update the View-Model
+        var value = this.element.value;
         this.bindings["valueInserted"].set(value);
-    }//,
-    
-    //change: function() {
-    //    var value = this.element.value;
-    //    update the View-Model
-    //    this.bindings["valueInserted"].set(value);
-    //}
+    }
 });
+
+//Version 1.0
+//kendo.data.binders.valueInserted = kendo.data.Binder.extend({
+//    
+//    init: function(element, bindings, options) {
+//        //call the base constructor
+//        kendo.data.Binder.fn.init.call(this, element, bindings, options);
+//        
+//        var that = this;
+        //listen for the change event of the element
+//        $(that.element).on("change", function() {
+            //call the change function
+//            that.change();
+//        });
+        
+        //listen for the keyup event of the element
+//        var binding = this.bindings.keyPress;
+//        $(that.element).bind("keyup", function(){
+//            delay(function(){
+                //call the change function
+//                that.change();
+//            }, 1000 );
+//        });
+//    },
+    
+//    refresh: function() {
+//        var that = this,
+            //get the value from the View-Model
+//            value = that.bindings["valueInserted"].get();
+        
+        //update the HTML input element
+//        $(that.element).val(value);
+//    },
+    
+//    change: function() {
+//        var value = this.element.value;
+        
+        //update the View-Model
+//        this.bindings["valueInserted"].set(value);
+//    }
+//});
 
 //View-Model source
 var viewModel = kendo.observable({
